eee8dd9f78aeed219dd54cfbde24aab1d8641765
Joe <joe@ou.studio>
2026-01-17 18:53:25 +0000

    s

--- app/db.server.ts
+++ app/db.server.ts
@@ -1,16 +1,27 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
 
 declare global {
   // eslint-disable-next-line no-var
-  var prismaGlobal: PrismaClient;
+  var prismaGlobal: PrismaClient | undefined;
 }
+
+function makePrisma() {
+  const connectionString = process.env.DATABASE_URL;
+  if (!connectionString) {
+    throw new Error("DATABASE_URL is not set");
+  }
+
+  const pool = new Pool({ connectionString });
+  const adapter = new PrismaPg(pool);
+  return new PrismaClient({ adapter });
+}
+
+export const prisma = global.prismaGlobal ?? makePrisma();
 
 if (process.env.NODE_ENV !== "production") {
-  if (!global.prismaGlobal) {
-    global.prismaGlobal = new PrismaClient();
-  }
+  global.prismaGlobal = prisma;
 }
-
-const prisma = global.prismaGlobal ?? new PrismaClient();
 
 export default prisma;
